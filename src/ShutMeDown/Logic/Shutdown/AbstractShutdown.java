30277d2e24f02b0bc9517abe956785507822c2aa
JuanMadness <sebastianrenzer@gmail.com>
2018-01-16 20:29:12 +0100

    -Created AbstractShutdown-Class -Override methodes in ShutdownWin

---
+++ src/ShutMeDown/Logic/Shutdown/AbstractShutdown.java
@@ -0,0 +1,20 @@
+package ShutMeDown.Logic.Shutdown;
+
+public abstract class AbstractShutdown {
+
+    public abstract void shutdown();
+
+    public abstract void cancelShutdown();
+
+    public void shutdownOnTime() {
+
+    }
+
+    public void shutdownCountdown() {
+
+    }
+
+    public void runCommand(final String pCommand) {
+
+    }
+}
--- src/ShutMeDown/Logic/Shutdown/ShutdownWin.java
+++ src/ShutMeDown/Logic/Shutdown/ShutdownWin.java
@@ -1,4 +1,16 @@
 package ShutMeDown.Logic.Shutdown;
 
-public class ShutdownWin {
+public class ShutdownWin extends AbstractShutdown {
+    @Override
+    public void shutdown() {
+        int tSeconds = 0; //Platzhalter
+        final String command = "shutdown -s -f -t " + tSeconds;
+        runCommand(command);
+    }
+
+    @Override
+    public void cancelShutdown() {
+        final String command = "shutdown -a";
+        runCommand(command);
+    }
 }
