4b8e54c9703fd75afae7708b5d59a40f9d230360
JuanMadness <sebastianrenzer@gmail.com>
2018-01-17 20:45:06 +0100

    -Fixed nogui-Mode

--- src/ShutMeDown/Start.java
+++ src/ShutMeDown/Start.java
@@ -6,23 +6,23 @@ import ShutMeDown.Logic.Shutdown.Shutdown;
 public class Start {
 
     public static void main(String args[]) {
-        if(args[1].equals("-nogui")) {
-            switch (args[2]){
+        if(args[0].equals("-nogui")) {
+            switch (args[1]){
                 case "timer":
-                    if(args.length == 5) {
+                    if(args.length == 4) {
                         int[] tTime = new int[2];
-                        tTime[0] = Integer.parseInt(args[3]);
-                        tTime[1] = Integer.parseInt(args[4]);
+                        tTime[0] = Integer.parseInt(args[2]);
+                        tTime[1] = Integer.parseInt(args[3]);
                         Shutdown.doShutdown(Shutdown.SHUTDOWN_TIMER, tTime);
                         System.out.println("Done!");
                     }
                     break;
                 case "countdown":
-                    if(args.length == 6) {
+                    if(args.length == 5) {
                         int[] tTime = new int[3];
-                        tTime[0] = Integer.parseInt(args[3]);
-                        tTime[1] = Integer.parseInt(args[4]);
-                        tTime[2] = Integer.parseInt(args[5]);
+                        tTime[0] = Integer.parseInt(args[2]);
+                        tTime[1] = Integer.parseInt(args[3]);
+                        tTime[2] = Integer.parseInt(args[4]);
                         Shutdown.doShutdown(Shutdown.SHUTDOWN_COUNTDOWN, tTime);
                         System.out.println("Done!");
                     }
